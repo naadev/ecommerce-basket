5c1cd29887d07bb67bf71484e61e7ddb6f24cbd8
Nico Afonso <sesa219774@se.com>
2021-03-15 13:33:48 +0100

    xUnit iteration 6

--- BasketTests/BasketTest.cs
+++ BasketTests/BasketTest.cs
@@ -11,35 +11,18 @@ namespace BasketTests
         {
             var basket = new Basket();
             Assert.True(basket.Total() == 0.0);
-        }
-        [Fact(DisplayName = "Given a basket with 1 item, And price for this item is 1.3, Then total ammount to pay will be 1.3")]
-        public void TestB()
+        } 
+        [Theory(DisplayName = "Given a basket of n items, and price for this item is x, total will be calculated")]
+        [InlineData(1.3, 1, 1.3)]
+        [InlineData(1.3, 2, 2.6)]
+        [InlineData(1.3, 3, 3.9)]
+        [InlineData(1.3, 4, 5.2)]
+        [InlineData(1.3, 5, 6.5)]
+        public void TestF(double price,int quantity,double total)
         {
             var basket = new Basket();
-            basket.AddItem(new Item { Price = 1.3 });
-            Assert.True(basket.Total() == 1.3);
+            basket.AddItem(new Item { Price = price }, quantity);
+            Assert.True(basket.Total() == total);
         }
-        [Fact(DisplayName = "Given a basket with 2 items, And price for this item is 1.3, Then total ammount to pay will be 2.6")]
-        public void TestC()
-        {
-            var basket = new Basket();
-            basket.AddItem(new Item { Price = 1.3 },2);
-            Assert.True(basket.Total() == 2.6);
-        }
-        [Fact(DisplayName = "Given a basket with 3 items, And price for this item is 1.3, Then total ammount to pay will be 3.9")]
-        public void TestD()
-        {
-            var basket = new Basket();
-            basket.AddItem(new Item { Price = 1.3 },3);
-            Assert.True(basket.Total() == 3.9);
-        }
-        [Fact(DisplayName = "Given a basket with 4 items, And price for this item is 1.3, Then total ammount to pay will be 3.9")]
-        public void TestE()
-        {
-            var basket = new Basket();
-            basket.AddItem(new Item { Price = 1.3 },4);
-            Assert.True(basket.Total() == 5.2);
-        }
-
     }
 }
