e9cf83acf9c82dd45283c018af95065f5d707efa
Nico Afonso <sesa219774@se.com>
2021-03-15 15:42:18 +0100

    xUnit iteration 9

--- BasketTests/Basket.cs
+++ BasketTests/Basket.cs
@@ -6,15 +6,18 @@ namespace BasketTests
 {
     internal class Basket
     {
-        public Basket()
+        double Fidelity { get; set; } = 0.0;
+        public Basket(double fidelity = 0.0)
         {
+            Fidelity = fidelity;
         }
 
         internal double Total()
         {
             double total = 0.0;
             Items.ForEach(x => total += (x.Price * (100 - x.Discount) / 100));
-            return Math.Round(total,2);
+            total = Math.Round(total*(100 - Fidelity)/100,2);
+            return total;
         }
         public List<Item> Items { get; set; } = new List<Item>();
         internal void AddItem(Item item, int quantity=1)
--- BasketTests/BasketTest.cs
+++ BasketTests/BasketTest.cs
@@ -6,13 +6,16 @@ namespace BasketTests
 {
     public class BasketTest
     {
-        [Fact(DisplayName = "Given an empty basket, Then total ammount to pay will be 0.0")]
+        [Fact(DisplayName = "Given an empty basket, " +
+            "Then total ammount to pay will be 0.0")]
         public void TestA()
         {
             var basket = new Basket();
             Assert.True(basket.Total() == 0.0);
         } 
-        [Theory(DisplayName = "Given a basket of n items, and price for this item is x, total will be calculated")]
+        [Theory(DisplayName = "Given a basket of n items, " +
+            "And price for this item is x, " +
+            "total will be calculated")]
         [InlineData(1.3, 1, 1.3)]
         [InlineData(1.3, 2, 2.6)]
         [InlineData(1.3, 3, 3.9)]
@@ -24,7 +27,10 @@ namespace BasketTests
             basket.AddItem(new Item { Price = price }, quantity);
             Assert.True(basket.Total() == total);
         }
-        [Theory(DisplayName = "Given a basket of n items, and price for this item is x, total will be calculated")]
+        [Theory(DisplayName = "Given a basket of n items, " +
+            "And price for this item is x, " +
+            "And there is an additional discount per item of y " +
+            "total will be calculated")]
         [InlineData(1.3, 1, 10, 1.17)]
         [InlineData(1.3, 2, 10, 2.34)]
         [InlineData(1.3, 3, 10, 3.51)]
@@ -35,6 +41,18 @@ namespace BasketTests
             var basket = new Basket();
             basket.AddItem(new Item { Price = price, Discount = discount }, quantity);
             Assert.True(basket.Total() == total);
+        }
+        [Fact(DisplayName = "Given a fidelity discount of 10% " +
+            "And 2 items with value 2.0 and discount value of 50% " +
+            "And 1 item with value 4.0  and discount value of 25% " +
+            "Then total amount will be ")]
+        public void TestH()
+        {
+            var fidelity = 10.0;
+            var basket = new Basket(fidelity);
+            basket.AddItem(new Item { Discount = 50.0, Price = 2.0 });
+            basket.AddItem(new Item { Discount = 25.0, Price = 4.0 });
+            Assert.True(basket.Total() == 3.6);
         }
     }
 }
